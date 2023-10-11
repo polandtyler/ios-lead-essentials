c9c5d44f092be717bd5768f76d7c8e0cee744de8
Tyler Poland <poland.tyler@gmail.com>
2023-10-11 15:34:27 -0700

    request data from url on load

--- EssentialDev_FeedProjectTests/RemoteFeedLoaderTests.swift
+++ EssentialDev_FeedProjectTests/RemoteFeedLoaderTests.swift
@@ -8,28 +8,47 @@
 import XCTest
 
 class HTTPClient {
-//    static let shared = HTTPClient()
-//
-//    private init() {}
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoader {
-    func load() {}
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://an-example-url.com")!
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient()
         let sut = RemoteFeedLoader()
+        
+        // ways of doing DI
+        // 1. method injection, like:
+        //      sut.load(client: client)
+        // 2. constructor/init injection, like:
+        //      RemoteFeedLoader(client: client)
+        // 3. property injection
+        //      let client = HTTPClient()
+        //      client.requestedURL = URL(string: "https://an-example-url.com")
+        
+        // but there's also a concrete way (probably not recommended for most scenarios:
+         let client = HTTPClient.shared // 🤢 - Why would this need to be a singleton? I could (should) have more than one HTTP Client
+        
+        
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
