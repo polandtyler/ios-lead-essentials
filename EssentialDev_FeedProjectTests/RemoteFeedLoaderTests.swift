b89174aac24426239bbd4f62112daebdbb32226b
Tyler Poland <poland.tyler@gmail.com>
2023-10-12 09:59:47 -0700

    refactor test to remove duplication

--- EssentialDev_FeedProjectTests/RemoteFeedLoaderTests.swift
+++ EssentialDev_FeedProjectTests/RemoteFeedLoaderTests.swift
@@ -11,17 +11,6 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        // this logic moved out of RemoteFeedLoader and into HTTPClient
-        requestedURL = url
-    }
-    
-}
-
 class RemoteFeedLoader {
     let client: HTTPClient
     let url: URL
@@ -39,25 +28,13 @@ class RemoteFeedLoader {
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init() {
-        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        _ = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        // ways of doing DI
-        // 1. method injection, like:
-        //      sut.load(client: client)
-        // 2. constructor/init injection, like:
-        //      RemoteFeedLoader(client: client)
-        // 3. property injection
-        //      let client = HTTPClient()
-        //      client.requestedURL = URL(string: "https://an-example-url.com")
-        
-        // but there's also a concrete way (probably not recommended for most scenarios:
-//         let client = HTTPClient. // 🤢 - Why would this need to be a singleton? I could (should) have more than one HTTP Client
         let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -65,6 +42,24 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClient) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            // this logic moved out of RemoteFeedLoader and into HTTPClient
+            requestedURL = url
+        }
+        
     }
 
 }
