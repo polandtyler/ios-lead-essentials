627d974e46257cddd45fe387e0ee58608ed629e8
Tyler Poland <poland.tyler@gmail.com>
2023-10-12 11:19:53 -0700

    improve test name

--- EssentialDev_FeedProjectTests_old/RemoteFeedLoaderTests.swift
+++ EssentialDev_FeedProjectTests_old/RemoteFeedLoaderTests.swift
@@ -17,7 +17,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
