f72f4736b44ee65575c245852c36568c34418173
Tyler Poland <poland.tyler@gmail.com>
2023-10-12 11:13:56 -0700

    move RemoteFeedLoader to production

--- EssentialDev_FeedProjectTests_old/RemoteFeedLoaderTests.swift
+++ EssentialDev_FeedProjectTests_old/RemoteFeedLoaderTests.swift
@@ -6,24 +6,7 @@
 //
 
 import XCTest
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
+import EssentialDev_FeedProject
 
 final class RemoteFeedLoaderTests: XCTestCase {
     
