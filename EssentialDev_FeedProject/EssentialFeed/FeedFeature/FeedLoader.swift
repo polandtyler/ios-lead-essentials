af30a9141533ae62d553a6e56a7ebe2f79c89f56
Tyler Poland <poland.tyler@gmail.com>
2023-10-19 14:41:35 -0700

    replace remotefeedloader.result enum with a predefined loadfeedresult enum

---
+++ EssentialDev_FeedProject/EssentialFeed/FeedFeature/FeedLoader.swift
@@ -0,0 +1,21 @@
+//
+//  FeedLoader.swift
+//  EssentialDev_FeedProject
+//
+//  Created by Poland, Tyler on 10/19/23.
+//
+
+import Foundation
+
+public enum LoadFeedResult<Error: Swift.Error> {
+	case success([FeedItem])
+	case failure(Error)
+}
+
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
+protocol FeedLoader {
+	associatedtype Error: Swift.Error
+	
+	func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
+}
