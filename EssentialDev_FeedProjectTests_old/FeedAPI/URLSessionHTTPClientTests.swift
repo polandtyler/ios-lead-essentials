503f67fd222c2a204b74b877c651674658ce6601
Tyler Poland <poland.tyler@gmail.com>
2023-10-29 20:49:18 -0700

    add memleak tracking

--- EssentialDev_FeedProjectTests_old/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialDev_FeedProjectTests_old/FeedAPI/URLSessionHTTPClientTests.swift
@@ -77,8 +77,18 @@ final class URLSessionHTTPClientTests: XCTestCase {
 	
 	// MARK: - TEST HELPERS
 	
-	private func makeSUT() -> URLSessionHTTPClient {
-		return URLSessionHTTPClient(session: URLSession.shared)
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
+		let sut = URLSessionHTTPClient(session: URLSession.shared)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return sut
+	}
+	
+	private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "Instance should have been dealloc'ed. Potential memory leak.",
+						 file: file,
+						 line: line)
+		}
 	}
 	
 	private class URLProtocolStub: URLProtocol {
