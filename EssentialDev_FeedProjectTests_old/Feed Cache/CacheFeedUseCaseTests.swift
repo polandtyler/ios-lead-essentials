8c8f5bdbb81ea2503cb0ccaf1d5eba11c67623c2
Tyler Poland <poland.tyler@gmail.com>
2023-11-26 12:29:27 -0600

    Implements the ability to save FeedItems with a FeedStore

--- EssentialDev_FeedProjectTests_old/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialDev_FeedProjectTests_old/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import EssentialDev_FeedProject
 
 class FeedStore {
 	var deleteCachedFeedCallCount = 0
@@ -17,6 +18,10 @@ class LocalFeedLoader {
 	init(store: FeedStore) {
 		self.store = store
 	}
+	
+	func save(items: [FeedItem]) {
+		store.deleteCachedFeedCallCount += 1
+	}
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -26,6 +31,24 @@ final class CacheFeedUseCaseTests: XCTestCase {
 		let _ = LocalFeedLoader(store: store)
 		
 		XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+	}
+	
+	func test_save_requestsCacheDeletion() {
+		let store = FeedStore()
+		let sut = LocalFeedLoader(store: store)
+		
+		sut.save(items: uniqueItems())
+		XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+	}
+	
+	// MARK: - Helpers
+	private func uniqueItems() -> [FeedItem] {
+		return [
+			FeedItem(id: UUID(),
+						description: "",
+						location: nil,
+						imageURL: URL(string: "http://a-url.com")!)
+			]
 	}
 
 }
