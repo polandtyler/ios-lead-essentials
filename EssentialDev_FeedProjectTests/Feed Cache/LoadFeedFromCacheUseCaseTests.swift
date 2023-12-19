a1ac9cbd8a4de5f18a17f9146433311ff0c2ef0b
Tyler Poland <poland.tyler@gmail.com>
2023-12-19 10:54:46 -0800

    Load command delivers no images on more than seven days old cache

--- EssentialDev_FeedProjectTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialDev_FeedProjectTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -63,6 +63,17 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		}
 	}
 	
+	func test_load_deliversNoImagesOnMoreThan7DayOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let sevenDayOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		
+		expect(sut, toCompleteWith: .success([])) {
+			store.completeRetrieval(with: [], timestamp: sevenDayOldTimestamp)
+		}
+	}
+	
 	/* ================================================================================================== */
 	// MARK: - HELPERS
 	/* ================================================================================================== */
