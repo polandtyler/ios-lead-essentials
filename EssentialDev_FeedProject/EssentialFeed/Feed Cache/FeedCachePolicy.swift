0ebb938a86500b119d97456bf5fbbc41f6495b8e
Tyler Poland <poland.tyler@gmail.com>
2024-01-02 11:19:01 -0800

    Move `FeedCachePolicy` to its own file

---
+++ EssentialDev_FeedProject/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialDev_FeedProjectTests
+//
+//  Created by Poland, Tyler on 1/2/24.
+//
+
+import Foundation
+
+internal enum FeedCachePolicy {
+	private static let calendar = Calendar(identifier: .gregorian)
+	
+	private static var maxCacheAgeInDays: Int {
+		return 7
+	}
+	
+	static func validate(_ timestamp: Date, against date: Date) -> Bool {
+		guard let maxCacheAge = calendar.date(byAdding: .day,
+											  value: maxCacheAgeInDays,
+											  to: timestamp) else {
+			return false
+		}
+		return date < maxCacheAge
+	}
+}
