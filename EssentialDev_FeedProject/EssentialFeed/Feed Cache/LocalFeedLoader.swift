3bea9fd1eb9ddb8d268e68ef7c5451bfba867a32
Tyler Poland <poland.tyler@gmail.com>
2024-01-02 11:14:44 -0800

    Fix whoopsie

--- EssentialDev_FeedProject/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialDev_FeedProject/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -17,7 +17,9 @@ private final class FeedCachePolicy {
 	static func validate(_ timestamp: Date, against date: Date) -> Bool {
 		guard let maxCacheAge = calendar.date(byAdding: .day,
 											  value: maxCacheAgeInDays,
-											  to: timestamp) else { return false }
+											  to: timestamp) else {
+			return false
+		}
 		return date < maxCacheAge
 	}
 	
@@ -44,10 +46,13 @@ extension LocalFeedLoader: FeedLoader {
 			case let .failure(error):
 				completion(.failure(error))
 				
-			case let .found(feed, timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
+			case let .found(feed, timestamp) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
 				completion(.success(feed.toModels()))
 				
-			case .empty, .found:
+			case .empty:
+				completion(.success([]))
+				
+			case .found:
 				completion(.success([]))
 			}
 		}
--- EssentialDev_FeedProjectTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialDev_FeedProjectTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -45,7 +45,7 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		let feed = uniqueImageFeed()
 		let fixedCurrentDate = Date()
 		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1 )
+		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
 		
 		expect(sut, toCompleteWith: .success(feed.models)) {
 			store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
