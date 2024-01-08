bd5445899b763a3d90d8d9f178415415e56a5fa9
Tyler Poland <poland.tyler@gmail.com>
2024-01-07 19:47:07 -0800

    Bringing everything to date

--- EssentialDev_FeedProject/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialDev_FeedProject/EssentialFeed/Feed Cache/FeedStore.swift
@@ -22,3 +22,20 @@ public enum RetrieveCachedFeedResult {
 	case found(feed: [LocalFeedImage], timestamp: Date)
 	case failure(Error)
 }
+
+/*
+ - Retrieve
+	- Empty cache
+	- non-empty cache returns data
+	- Non-empty cache twice returns the same data (no side-effects)
+	- Error (if applicable - ex: invalid data)
+ - Insert
+	- To empty cache
+	- To non-empty cache overrides previous data with new data
+	- Error (if applicable - ex: no write permission, out of space, etc)
+ - Delete
+	- Empty cache does nothing (cache stays empty and does not fail)
+	- Non-empty cache leaves cache empty
+	- Error (if applicable - ex: no delete permissions)
+ - Side effects must run serially to avoid race conditions
+ */
