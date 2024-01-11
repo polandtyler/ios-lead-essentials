8cdcf80a89538beea71b0631816b308c58713c60
Tyler Poland <poland.tyler@gmail.com>
2024-01-11 14:37:15 -0800

    Move `Codable` conformance from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFeedImage` type

--- EssentialDev_FeedProjectTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialDev_FeedProjectTests/Feed Cache/CodableFeedStoreTests.swift
@@ -13,8 +13,31 @@ typealias InsertionCompletion = (Error?) -> Void
 class CodableFeedStore {
 	
 	private struct Cache: Codable {
-		let feed: [LocalFeedImage]
+		let feed: [CodableFeedImage]
 		let timestamp: Date
+		
+		var localFeed: [LocalFeedImage] {
+			feed.map { $0.local }
+		}
+	}
+	
+	private struct CodableFeedImage: Codable {
+		private let id: UUID
+		private let description: String?
+		private let location: String?
+		private let url: URL
+		
+		var local: LocalFeedImage {
+			return LocalFeedImage(id: id, description: description, location: location, url: url)
+		}
+		
+		init(_ image: LocalFeedImage) {
+			id = image.id
+			description = image.description
+			location = image.location
+			url = image.url
+		}
+		
 	}
 	
 	private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
@@ -27,13 +50,14 @@ class CodableFeedStore {
 		
 		let decoder = JSONDecoder()
 		let cache = try! decoder.decode(Cache.self, from: data)
-		completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+		completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
 		
 	}
 	
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		let encoder = JSONEncoder()
-		let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+		let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+		let encoded = try! encoder.encode(cache)
 		try! encoded.write(to: storeURL)
 		completion(nil)
 	}
